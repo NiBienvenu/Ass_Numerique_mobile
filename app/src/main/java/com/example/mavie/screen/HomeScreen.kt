96edb64b022e4c377b9990c52c430636ddc3284e
NiBienvenu <nicibienvenu@gmail.com>
2024-08-26 08:52:44 +0200

    home screen

--- app/src/main/java/com/example/mavie/screen/HomeScreen.kt
+++ app/src/main/java/com/example/mavie/screen/HomeScreen.kt
@@ -1,4 +1,70 @@
 package com.example.mavie.screen
 
-class HomeScreen {
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Notifications
+import androidx.compose.material.icons.filled.Person
+import androidx.compose.material.icons.outlined.Notifications
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.mavie.ui.theme.primary
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Preview(showBackground = true, showSystemUi = true)
+@Composable
+fun HomeScreen(){
+
+    Scaffold(
+        topBar ={
+            TopAppBar(
+                modifier = Modifier
+                    .padding(10.dp)
+                    .clip(RoundedCornerShape(20)),
+                colors = TopAppBarDefaults.topAppBarColors(
+                    containerColor = MaterialTheme.colorScheme.outline.copy(0.4f)
+                ),
+                title = {
+                Text(text = "Assurance numerique", fontSize = 18.sp)
+            },
+                actions = {
+                    Icon(
+                        imageVector = Icons.Default.Notifications,
+                        contentDescription = null,
+                        modifier = Modifier
+                            .padding(end = 20.dp)
+                            .size(28.dp)
+                    )
+                    Icon(
+                        imageVector = Icons.Default.Person,
+                        contentDescription = null,
+                        modifier = Modifier
+                            .padding(end = 20.dp)
+                            .size(28.dp)
+                    )
+                }
+
+            )
+        },
+
+    ) { paddingValues ->
+        Box(modifier = Modifier
+            .padding(paddingValues)){
+
+        }
+    }
 }
